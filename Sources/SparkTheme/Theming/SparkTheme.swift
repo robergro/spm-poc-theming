028da9c76c2e00b62e2b4401c89f8128a09af894
Robin Lemaire <robin.lemaire@adevinta.com>
2024-02-22 10:51:09 +0100

    Test CI Linter

--- Sources/SparkTheme/Theming/SparkTheme.swift
+++ Sources/SparkTheme/Theming/SparkTheme.swift
@@ -10,18 +10,30 @@ import Combine
 import SparkTheming
 import Foundation
 
+
+
+
+
 public struct SparkTheme: Theme {
 
     // MARK: - Properties
 
     public static var shared = Self()
 
-    public init() {}
+    public init()  {}
+
+
+
+
+
+
+
+
 
     public let border: Border = SparkBorder()
-    public var colors: Colors = SparkColors()
+    public var colors: Colors =   SparkColors()
     public let elevation: Elevation = SparkElevation()
-    public let layout: Layout = SparkLayout()
+    public let layout: Layout =  SparkLayout()
     public let typography: Typography = SparkTypography()
-    public let dims: Dims = SparkDims()
+    public let dims: Dims =    SparkDims()
 }
